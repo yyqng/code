b0264ae27a03807d3aa55f28a588e71936de4550
yyqng <yyqng@qq.com>
2019-09-14 06:52:24 +0800

    new file:   linux0.11 	new file:   lua/lua-5.2.2/lua.hpp 	modified:   lua/luaapi/Makefile 	modified:   lua/luaapi/api.cpp

--- lua/luaapi/api.cpp
+++ lua/luaapi/api.cpp
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <cstring>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <lua.hpp>
 using namespace std;
-extern "C"{
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
-}
 
 static void stackDump(lua_State *L){
     int i;
@@ -13,14 +12,14 @@ static void stackDump(lua_State *L){
     for(i = 1; i <= top; i++){
         int t = lua_type(L, i);
         switch(t){
-            case LUA_TSTRING:
-                printf("'%s'", lua_tostring(L, i));
-                break;
             case LUA_TBOOLEAN:
                 printf(lua_toboolean(L, i) ? "true":"false");
                 break;
             case LUA_TNUMBER:
                 printf("%g", lua_tonumber(L, i));
+                break;
+            case LUA_TSTRING:
+                printf("'%s'", lua_tostring(L, i));
                 break;
             default:
                 printf("%s", lua_typename(L, t));
@@ -30,15 +29,14 @@ static void stackDump(lua_State *L){
     }
     printf("\n");
 }
-int test(void){
 
+void test(void){
     lua_State *L = luaL_newstate();
 
     lua_pushboolean(L, 1);
     lua_pushnumber(L, 10);
     lua_pushnil(L);
     lua_pushstring(L, "yoyu");
-
     stackDump(L);    // dump the stack
 
     lua_pushvalue(L, -4);// push the value of the index to the stack
@@ -57,7 +55,6 @@ int test(void){
     stackDump(L);
 
     lua_close(L);
-    return 0;
 }
 /**/
 int test1(void)
@@ -84,8 +81,56 @@ int test1(void)
     return 0;
 }
 
+void printFloats (int n, ...)
+{
+  int i;
+  double val;
+  printf ("Printing floats:");
+  va_list vl;
+  va_start(vl,n);
+  for (i = 0; i < n; i++)
+  {
+    val = va_arg(vl,double);
+    printf (" [%.2f]",val);
+  }
+  va_end(vl);
+  printf ("\n");
+}
+
+void error (lua_State *L, const char *fmt, ...) {
+    va_list vl;
+    va_start(vl, fmt);
+    vfprintf(stderr, fmt, vl);
+    va_end(vl);
+    lua_close(L);
+    printf ("\n");
+    exit(EXIT_FAILURE);
+    printf ("\n");
+}
+
+void test2(void)
+{
+    const char *fmt="%s";
+    const char *s="hello";
+    //int i =5;
+    lua_State *L = luaL_newstate();//lua_open();
+    error(L,fmt,s);
+}
+
+void test3(void)
+{
+    lua_State *L = luaL_newstate();
+    lua_pushstring(L, "yoyu");
+    const char *s = lua_tostring(L, -1); /* any Lua string */
+    size_t l = luaL_len(L, -1); /* its length */
+    assert(s[l-1] == '\0');
+    assert(strlen(s) <= l);
+}
+
 int main(void)
 {
-    test();
+    //test();
     //test1();
+    //printFloats (3,3.14159,2.71828,1.41421);
+    //test2();
 }
