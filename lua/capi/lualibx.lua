5877b9a11d2295767432d217f5c2c2af26593c81
yyqng <yyqng@qq.com>
2019-09-17 20:49:16 +0800

    capi

--- lua/capi/lualibx.lua
+++ lua/capi/lualibx.lua
@@ -1,7 +1,9 @@
 --Name of this file can't be lualib.lua because of lualib.so
 local lib = require "mylib"
+--local lib = package.loadlib("mylib", "luaopen_mylib")
+--lib()
 print(type(lib))
-print(lib.sum(23,17))
+print(lib.l_sum(23,17))
 print(lib.l_sin(3.14159265354))
 print(mysin(3.14159265354))
---print(lib.l_sin(3.141592653))
+print(lib.l_dir("."))
--- lua/capi/mylib.c
+++ lua/capi/mylib.c
@@ -2,6 +2,7 @@
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
+#include <string.h>
  
 static int sum(lua_State *L){
     double d1 = luaL_checknumber(L, 1);
@@ -10,15 +11,46 @@ static int sum(lua_State *L){
     return 1;
 }
 
-static int l_sin (lua_State *L) {
+static int mysin (lua_State *L) {
     double d = lua_tonumber(L, 1); /* get argument */
     lua_pushnumber(L, sin(d)); /* push result */
     return 1; /* number of results */
 }
+
+#include <dirent.h>
+#include <errno.h>
+static int dir (lua_State *L) {
+    DIR *dir;
+    struct dirent *entry;
+    int i;
+    const char *path = luaL_checkstring(L, 1);
+    /* open directory */
+    dir = opendir(path);
+    if (dir == NULL) {
+        lua_pushnil(L);
+        /* error opening the directory? */
+        /* return nil and ... */
+        lua_pushstring(L, strerror(errno)); /* error message */
+        return 2; /* number of results */
+    }
+    /* create result table */
+    lua_newtable(L);
+    i = 1;
+    while ((entry = readdir(dir)) != NULL) {
+        lua_pushnumber(L, i++); /* push key */
+        lua_pushstring(L, entry->d_name); /* push value */
+	//printf("entry->d_name : %s\n", entry->d_name);
+        lua_settable(L, -3);
+    }
+    closedir(dir);
+    return 1;
+    /* table is already on top */
+}
  
 static const struct luaL_Reg mylib[] = {
-    {"sum" , sum},
-    {"l_sin" , l_sin},
+    {"l_sum" , sum},
+    {"l_sin" , mysin},
+    {"l_dir" , dir},
     {NULL, NULL}
 };
  
