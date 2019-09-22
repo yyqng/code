5dd89e1379321c7ad9e76b02ecd1b29e0f956fa9
yyqng <yyqng@qq.com>
2019-09-22 17:33:31 +0800

    mylua

--- lua/capi/lualibx.lua
+++ lua/capi/lualibx.lua
@@ -1,18 +1,37 @@
 --Name of this file can't be lualib.lua because of lualib.so
 local lib = require "mylib"
---local lib = package.loadlib("mylib", "luaopen_mylib")
---lib()
-print(type(lib))
-print(lib.l_sum(23,17))
-print(lib.l_sin(3.14159265354))
-print(mysin(3.14159265354))
-print(lib.l_dir("."))
 
-mytable = {}
-mytable[1]= "element 1"
-mytable[1]= "element 2"
-mytable[2]= "element 2"
-myprint = function(param)
-   print("myprint:",param)
+l_mapTest = function()
+    mytable = {}
+    mytable[1]= "element 1"
+    mytable[2]= "element 2"
+    myprint = function(param)
+       print("myprint:",param)
+    end
+    print(lib.l_map(mytable, myprint))
 end
-print(lib.l_map(mytable, myprint))
+
+l_ctableTest = function(n)
+    ctable = lib.l_newarray(n)
+    for i=1,n do
+        lib.l_setarray(ctable, n, i + 100)
+    end
+        print(lib.l_getarray(ctable, 1))
+        print(lib.l_getarray(ctable, 2))
+    for i=1,n do
+        print(lib.l_getarray(ctable, i))
+    end
+    print("Size of ctable:", lib.l_getsize(ctable))
+end
+
+--print(type(lib))
+--print(lib.l_sum(23,17))
+--print(lib.l_sin(3.14159265354))
+--print(mysin(3.14159265354))
+--print(lib.l_dir("."))
+--l_mapTest() 
+--print(lib.l_split("hi,,there", ","))
+--print(lib.l_ctest())
+l_ctableTest(2)
+
+
--- lua/capi/mylib.c
+++ lua/capi/mylib.c
@@ -3,6 +3,8 @@
 #include <lauxlib.h>
 #include <lualib.h>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
  
 static int sum(lua_State *L){
     double d1 = luaL_checknumber(L, 1);
@@ -17,8 +19,6 @@ static int mysin (lua_State *L) {
     return 1; /* number of results */
 }
 
-#include <dirent.h>
-#include <errno.h>
 static int dir (lua_State *L) {
     DIR *dir;
     struct dirent *entry;
@@ -62,11 +62,117 @@ int map (lua_State *L) {
     }
     return 0; /* no results */
 }
+
+static int split (lua_State *L) {
+    const char *s = luaL_checkstring(L, 1);
+    const char *sep = luaL_checkstring(L, 2);
+    const char *e;
+    int i = 1;
+    lua_newtable(L); /* result */
+    /* repeat for each separator */
+    while ((e = strchr(s, *sep)) != NULL) {
+        lua_pushlstring(L, s, e-s); /* push substring */
+        lua_rawseti(L, -2, i++);
+        printf("%s\n",e);
+        s = e + 1; /* skip separator */
+    }
+    /* push last substring */
+    lua_pushstring(L, s);
+    lua_rawseti(L, -2, i);
+    return 1; /* return the table */
+}
+
+int ctest(lua_State *L)
+{
+
+    static const char kKey = 'K';
+    static const char kkkkk[] = "kkkkk";
+    /* store a string*/
+    lua_pushlightuserdata(L, (void *)&kKey); /* push address */
+    lua_pushstring(L, kkkkk);
+    /* push value */
+    /* registry[&kKey] = myNumber */
+    lua_settable(L, LUA_REGISTRYINDEX);
+
+    /* variable with an unique address */
+    static const char Key = 'k';
+    int myNumber = 1111;
+    /* store a number */
+    lua_pushlightuserdata(L, (void *)&Key); /* push address */
+    lua_pushnumber(L, myNumber);
+    /* push value */
+    /* registry[&Key] = myNumber */
+    lua_settable(L, LUA_REGISTRYINDEX);
+
+    /* retrieve a number */
+    lua_pushlightuserdata(L, (void *)&Key);
+    /* push address */
+    lua_gettable(L, LUA_REGISTRYINDEX); /* retrieve value */
+    myNumber = lua_tonumber(L, -1); /* convert to number */
+    printf("myNumber = %d\n", myNumber);
+
+    /* retrieve a string*/
+    lua_pushlightuserdata(L, (void *)&kKey);
+    /* push address */
+    lua_gettable(L, LUA_REGISTRYINDEX); /* retrieve value */
+    const char *s = lua_tostring(L, -1); /* convert to number */
+    printf("s = %s\n", s);
+
+    return 1; /* return the table */
+}
+
+typedef struct NumArray {
+    int size;
+    double values[1]; /* variable part */
+} NumArray;
+
+static int newarray (lua_State *L) {
+    int n = luaL_checkint(L, 1);
+    size_t nbytes = sizeof(NumArray) + (n - 1)*sizeof(double);
+    NumArray *a = (NumArray *)lua_newuserdata(L, nbytes);
+    a->size = n;
+    return 1; /* new userdatum is already on the stack */
+}
+
+static int setarray (lua_State *L) {
+    NumArray *a = (NumArray *)lua_touserdata(L, 1);
+    int index = luaL_checkint(L, 2);
+    double value = luaL_checknumber(L, 3);
+    luaL_argcheck(L, a != NULL, 1, "`array' expected");
+    luaL_argcheck(L, 1 <= index && index <= a->size, 2, "index out of range");
+    a->values[index-1] = value;
+    //printf("value is set to = %f\n", value);
+    return 0;
+}
+
+static int getarray (lua_State *L) {
+    NumArray *a = (NumArray *)lua_touserdata(L, 1);
+    int index = luaL_checkint(L, 2);
+    luaL_argcheck(L, a != NULL, 1, "'array' expected");
+    luaL_argcheck(L, 1 <= index && index <= a->size, 2,
+    "index out of range");
+    lua_pushnumber(L, a->values[index-1]);
+    return 1;
+}
+
+static int getsize (lua_State *L) {
+    NumArray *a = (NumArray *)lua_touserdata(L, 1);
+    luaL_argcheck(L, a != NULL, 1, "`array' expected");
+    lua_pushnumber(L, a->size);
+    return 1;
+}
+
 static const struct luaL_Reg mylib[] = {
     {"l_sum" , sum},
     {"l_sin" , mysin},
     {"l_dir" , dir},
     {"l_map" , map},
+    {"l_split" , split},
+    {"l_ctest" , ctest},
+    {"l_newarray" , newarray},
+    {"l_setarray" , setarray},
+    {"l_getarray" , getarray},
+    {"l_getsize" , getsize},
     {NULL, NULL}
 };
  
