c88b03c714e17e1f0b430866da87ce2dd0e99a88
root <root@localhost.localdomain>
2020-01-05 12:55:09 +0800

    new file:   c/Makefile 	new file:   c/test 	new file:   c/test.c 	modified:   lua/luaapi/api.cpp

---
+++ c/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>  
+#include <stdint.h>  
+#include <stdlib.h>  
+#include <math.h>  
+
+double erf_test()
+{
+    double r = erf(1.96*sqrt(1/2.));
+    double bottom_tail = gsl_cdf_gaussian_P(-1.96, 1);
+    printf("Area between [-1.96, 1.96]: %g\n", 1-2*bottom_tail);
+    double ret = r + 1.0;
+    return ret;
+}
+
+//reps=10 msg="Ha" ./test
+void print_env()
+{
+    char *repstext = getenv("reps");
+    int reps = repstext ? atoi(repstext) : 2;
+
+    char *msg = getenv("msg");
+    if (!msg) {
+        msg = "Hello.";
+    }
+
+    for (int i=0; i < reps; i++)
+        printf("%s\n", msg);
+}
+
+int main()
+{
+    erf_test();
+//   print_env();
+}
