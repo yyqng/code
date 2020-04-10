3cb9909127644db961868a9bed7dce680247790f
root <root@localhost.localdomain>
2020-04-10 09:14:24 +0800

    biastable

--- c/biastable.cpp
+++ c/biastable.cpp
@@ -1,17 +1,4 @@
-#include<stdio.h>
-#define TBX_EPSILON (1e-4)
-#define DT_MIN(a,b)        ((a) < (b) ? (a) : (b))
-#define DT_MAX(a,b)        ((a) >= (b) ? (a) : (b))
-
-#define TBX_MAX(x, y) DT_MAX(x, y)
-#define TBX_MIN(x, y) DT_MIN(x, y)
-#define TBX_LE( x, y ) (( (x) <= (y) + TBX_EPSILON) ? 1 : 0) 
-#define TBX_GE( x, y ) (( (x) >= (y) - TBX_EPSILON ) ? 1 : 0) 
-#define TBX_LT( x, y ) (( (x) < (y) - TBX_EPSILON) ? 1 : 0) 
-#define TBX_GT( x, y ) (( (x) > (y) + TBX_EPSILON ) ? 1 : 0)
-#define TBX_EQ( x, y ) (( (x) <= (y) + TBX_EPSILON && (x) >= (y) - TBX_EPSILON ) ? 1: 0)
-#define TBX_NE( x, y ) (( (x) < (y) - TBX_EPSILON || (x) > (y) + TBX_EPSILON ) ? 1: 0)
-
+#include"biasTable.h"
 /*
 Use function: F(x, y)=a*x+b to match
 F(x[0])=v[0]
@@ -21,16 +8,18 @@ Return: F(x0)
 */
 static double tbx1DInterpolation(double x[2], double v[2], double x0)
 {
-  if(TBX_EQ(v[0], v[1])){return v[0];}
-  if(TBX_EQ(x[0], x[1])){
-    printf("tbx1DInterpolation can not handle this case!\n");
-  }
-  double a=v[1]-v[0];
-  double b=v[0]*x[1]-v[1]*x[0];
-  double ret=a*x0+b;
-  ret/=(x[1]-x[0]);
+    if (TBX_EQ(v[0], v[1])) {
+        return v[0];
+    }
+    if (TBX_EQ(x[0], x[1])) {
+        printf("tbx1DInterpolation can not handle this case!\n");
+    }
+    double a = v[1] - v[0];
+    double b = v[0] * x[1] - v[1] * x[0];
+    double ret = a * x0 + b;
+    ret /= (x[1] - x[0]);
 
-  return ret;
+    return ret;
 }
 
 /*
@@ -44,36 +33,44 @@ Return: F(x0, y0)
 */
 double tbx2DInterpolation(double x[2], double y[2], double v[2][2], double x0, double y0)
 {
-  if(TBX_EQ(x[0], x[1])){
-    if(TBX_EQ(v[0][0], v[1][0]) && TBX_EQ(v[0][1], v[1][1])){
-      double temp[2]={v[0][0], v[0][1]};
-      return tbx1DInterpolation(y, temp, y0);
-    }else{
-      printf("tbx2DInterpolation can not handle this case!\n");
+    if (TBX_EQ(x[0], x[1])) {
+        if (TBX_EQ(v[0][0], v[1][0]) && TBX_EQ(v[0][1], v[1][1])) {
+            double temp[2] = {v[0][0], v[0][1]};
+            return tbx1DInterpolation(y, temp, y0);
+        } else {
+            printf("tbx2DInterpolation can not handle this case!\n");
+        }
     }
-  }
-  if(TBX_EQ(y[0], y[1])){
-    if(TBX_EQ(v[0][0], v[0][1]) && TBX_EQ(v[1][0], v[1][1])){
-      double temp[2]={v[0][0], v[1][0]};
-      return tbx1DInterpolation(x, temp, x0);
-    }else{
-      printf("tbx2DInterpolation can not handle this case!\n");
+    if (TBX_EQ(y[0], y[1])) {
+        if (TBX_EQ(v[0][0], v[0][1]) && TBX_EQ(v[1][0], v[1][1])) {
+            double temp[2] = {v[0][0], v[1][0]};
+            return tbx1DInterpolation(x, temp, x0);
+        }else{
+            printf("tbx2DInterpolation can not handle this case!\n");
+        }
     }
-  }
 
-  if(TBX_EQ(x0, x[0]) && TBX_EQ(v[0][0], v[0][1])){return v[0][0];}
-  if(TBX_EQ(x0, x[1]) && TBX_EQ(v[1][0], v[1][1])){return v[1][0];}
-  if(TBX_EQ(y0, y[0]) && TBX_EQ(v[0][0], v[1][0])){return v[0][0];}
-  if(TBX_EQ(y0, y[1]) && TBX_EQ(v[0][1], v[1][1])){return v[0][1];}
+    if (TBX_EQ(x0, x[0]) && TBX_EQ(v[0][0], v[0][1])) {
+        return v[0][0];
+    }
+    if (TBX_EQ(x0, x[1]) && TBX_EQ(v[1][0], v[1][1])) {
+        return v[1][0];
+    }
+    if (TBX_EQ(y0, y[0]) && TBX_EQ(v[0][0], v[1][0])) {
+        return v[0][0];
+    }
+    if (TBX_EQ(y0, y[1]) && TBX_EQ(v[0][1], v[1][1])) {
+        return v[0][1];
+    }
 
-  double a=v[0][0]-v[0][1]-v[1][0]+v[1][1];
-  double b=(v[1][0]-v[0][0])*y[1]-(v[1][1]-v[0][1])*y[0];
-  double c=(v[0][1]-v[0][0])*x[1]-(v[1][1]-v[1][0])*x[0];
-  double d=x[1]*(y[1]*v[0][0]-y[0]*v[0][1])-x[0]*(y[1]*v[1][0]-y[0]*v[1][1]);
-  double ret=(a*y0+b)*x0+c*y0+d;
-  ret/=((x[1]-x[0])*(y[1]-y[0]));
+    double a = v[0][0] - v[0][1] - v[1][0] + v[1][1];
+    double b = (v[1][0] - v[0][0]) * y[1] - (v[1][1] - v[0][1]) * y[0];
+    double c = (v[0][1] - v[0][0]) * x[1]-(v[1][1] - v[1][0]) * x[0];
+    double d = x[1] * (y[1] * v[0][0] - y[0] * v[0][1]) - x[0] * (y[1]*v[1][0] - y[0] * v[1][1]);
+    double ret = (a * y0 + b) * x0 + c * y0 + d;
+    ret /= ((x[1] - x[0]) * (y[1] - y[0]));
 
-  return ret;
+    return ret;
 }
 
 /********************************************************************
@@ -95,17 +92,27 @@ int tbxSearchTable(
         int if_ceil     /* input: if (!if_ceil) --> use ">" */
 )
 {   
-     int high=nmemb, low=-1;
-     if(!d) {return low;}
-     while(high - low > 1) {
-       int probe=(high + low) / 2;
-       if (!if_ceil) {
-         if(d[probe]>key) {high = probe;} else {low = probe;}
-       } else {
-         if(d[probe]>=key) {high = probe;} else {low = probe;}
-       }
-     }
-     return low;
+    int high = nmemb, low = -1;
+    if (!d) {
+        return low;
+    }
+    while(high - low > 1) {
+        int probe = (high + low) / 2;
+        if (!if_ceil) {
+            if (d[probe] > key) {
+                high = probe;
+            } else {
+                low = probe;
+            }
+        } else {
+            if (d[probe] >= key) {
+                high = probe;
+            } else {
+                low = probe;
+            }
+        }
+    }
+    return low;
 }
 
 /******************************************************************
@@ -171,6 +178,67 @@ double tbxCalBias(
     return bias;
 }
 
+/******************************************************************
+ * 
+ * tbxCalValueBy2DTable
+ *
+ * Compute value from table2D and valueTable
+ * 
+ * Return value
+ *******************************************************************/
+double tbxCalValueBy2DTable(
+        double x, double y,
+        int *doInterpolation,
+        double *table2D[2], int len[2],
+        double *valueTable
+)
+{
+    double bias = 0;
+    if (!valueTable || !table2D[1] || !table2D[0]) {
+        return bias;
+    }
+    int wi = tbxSearchTable(table2D[0], len[0], x, 0);
+    if (wi < 0) {
+        return bias;
+    }
+    int si = tbxSearchTable(table2D[1], len[1], y, 0);
+    if (si < 0) {
+        return bias;
+    }
+    if (1 == doInterpolation[0] && 1 == doInterpolation[1]) {
+        int nwi = TBX_MIN(wi + 1, len[0] - 1);
+        int nsi = TBX_MIN(si + 1, len[1] - 1);
+        double x_temp[2] = {table2D[1][si], table2D[1][nsi]};
+        double y_temp[2] = {table2D[0][wi], table2D[0][nwi]};
+        double v[2][2] = {{valueTable[si * len[0] + wi], valueTable[si * len[0] + nwi]}, 
+                          {valueTable[nsi * len[0] + wi], valueTable[nsi * len[0] + nwi]}};
+        bias = tbx2DInterpolation(x_temp, y_temp, v, y, x);
+    } else if (doInterpolation[0] == 1 && wi < len[0] - 1) {
+        double bias1 = valueTable[si * len[0] + wi];
+        double bias2 = valueTable[si * len[0] + wi + 1];
+        double v[2] = {valueTable[si * len[0] + wi], valueTable[si * len[0] + wi + 1]};
+        double ret = tbx1DInterpolation(&table2D[0][wi], v, x);
+        double t = (y - table2D[1][si]) / (table2D[1][si + 1] - table2D[1][si]);
+        bias = bias1 * (1 - t) + bias2 * t;
+        if(TBX_NE(ret, bias)) {
+            printf("ret and bias is not equal.");
+        }
+    } else if (doInterpolation[1] == 1 && si < len[1] - 1) {
+        double bias1 = valueTable[si * len[0] + wi];
+        double bias2 = valueTable[(si + 1) * len[0] + wi];
+        double t = (y - table2D[1][si]) / (table2D[1][si + 1] - table2D[1][si]);
+        bias = bias1 * (1 - t) + bias2 * t;
+        double v[2] = {valueTable[si * len[0] + wi], valueTable[(si + 1) * len[0] + wi]};
+        double ret = tbx1DInterpolation(&table2D[1][si], v, y);
+        if(TBX_NE(ret, bias)) {
+            printf("ret and bias is not equal.");
+        }
+    } else {
+        bias = valueTable[si * len[0] + wi];
+    }
+    return bias;
+}
+
 
 /******************************************************************
  * 
@@ -188,17 +256,17 @@ static double tbxCalValueBy3DTable(
 )
 {
     double ret = 0;
-    if(!valueTable || !table3D[1] || !table3D[0]) {
+    if (!valueTable || !table3D[1] || !table3D[0]) {
         return ret;
     }
-    if(!table3D[2]) {
+    if (!table3D[2]) {
         ret = tbxCalBias(y, z, interpolation[1], table3D[0], len[2], table3D[1], len[1], valueTable[0]);
         return ret;
     }
     int li = tbxSearchTable(table3D[2], len[0], x, 0);
-    if( li < 0 ) {
+    if (li < 0) {
         return 0;
-    } else if(0 == interpolation[0] || li >= len[0] -1 ) { /* no interpolation for lengthTable*/
+    } else if (0 == interpolation[0] || li >= len[0] -1 ) { /* no interpolation for lengthTable*/
         ret = tbxCalBias(y, z, interpolation[1], table3D[0], len[2], table3D[1], len[1], valueTable[li]);
         return ret;
     } else {
@@ -209,6 +277,34 @@ static double tbxCalValueBy3DTable(
     }
     return ret;
 }
+
 int main()
 {
+    int len[3] = {3, 3, 3};
+    double *table3D[3];
+    //for (int i = 0; i < TABLE_NUMBERS; ++i) {
+    for (int i = 0; i < 1; ++i) {
+        table3D[0] = T1D[i];
+        table3D[1] = T2D[i];
+        table3D[2] = T3D[i];
+        for (int j = 0; j < CASE_SIZE; ++j) {
+            double x = testdata[i][0][j][0];
+            double y = testdata[i][0][j][1];
+            double z = testdata[i][0][j][2];
+            printf("(x, y, z) = (%f, %f, %f)\n", x, y, z);
+            for (int k = 0; k < interpolation_size; ++k) {
+                x = testdata[i][k][j][0];
+                y = testdata[i][k][j][1];
+                z = testdata[i][k][j][2];
+                double r = testdata[i][k][j][3];
+                double **biasTable = values;
+                double ret = tbxCalValueBy3DTable(x, y, z, interpolation[i], table3D, len, biasTable);
+                if (TBX_NE(ret, r)) {
+                    printf("error : (i, j, k) = (%d, %d, %d), r = %f, ret = %f\n", i, j, k, r, ret);
+                }
+            }
+            printf("-----------------------\n");
+        }
+        printf("=======================\n");
+    }
 }
