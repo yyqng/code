4c0dee9b5392df222b2d8e009d3cb22390ebe3e1
yyqng <yyqng@qq.com>
2020-05-17 20:51:41 +0800

    modified:   Makefile 	new file:   otst.cpp 	modified:   test.cpp 	new file:   test.h

---
+++ cppprimer/test.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include <iterator>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <cstring>
+#include <deque>
+#include <iterator>
+//g++ primer.cpp -g -std=c++11
+using namespace std;
+void algo();
+int otst();
