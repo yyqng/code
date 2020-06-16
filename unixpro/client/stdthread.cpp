28734d6d8ca0f200073b1643b66463beee69cc6a
yyqng <yyqng@qq.com>
2020-06-16 22:54:19 +0800

    modified:   client/stdthread.cpp

--- unixpro/client/stdthread.cpp
+++ unixpro/client/stdthread.cpp
@@ -5,7 +5,7 @@ const int SIZE = 10;
 void f1(int n)
 {
     for (int i = 0; i < 5; ++i) {
-        std::cout << "Thread " << n << " executing\n";
+        std::cout << "f1: Thread 1 n = " << n << " executing\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
@@ -13,19 +13,19 @@ void f1(int n)
 void f2(int& n)
 {
     for (int i = 0; i < 5; ++i) {
-        std::cout << "Thread 2 executing\n";
         ++n;
+        std::cout << "f2: Thread 2 n = " <<  n << " executing\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
 
 int test1()
 {
+    std::thread t1;                   // t1 is not a thread
+    std::thread t2(f1, 1);            // pass by value
     int n = 0;
-    std::thread t1; // t1 is not a thread
-    std::thread t2(f1, n + 1); // pass by value
-    std::thread t3(f2, std::ref(n)); // pass by reference
-    std::thread t4(std::move(t3)); // t4 is now running f2(). t3 is no longer a thread
+    std::thread t3(f2, std::ref(n));  // pass by reference
+    std::thread t4(std::move(t3));    // t4 is now running f2(). t3 is no longer a thread
     t2.join();
     t4.join();
     std::cout << "Final value of n is " << n << '\n';
@@ -149,7 +149,7 @@ void threadCaller()
     std::thread t(independentThread);
     t.detach();
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "Exiting thread caller.\n";
+    std::cout << "Exiting thread caller. t.joinable() = " << t.joinable() << "\n";
 }
  
 int test6() 
@@ -283,7 +283,7 @@ int test11()
 {
     auto start = std::chrono::high_resolution_clock::now();
   
-    little_sleep(std::chrono::microseconds(100));
+    little_sleep(std::chrono::microseconds(1000000));
   
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
     std::cout << "waited for "
@@ -329,5 +329,5 @@ int test13()
 
 void stdthread_test()
 {
-    test1();
+    test12();
 }
