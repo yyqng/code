dcc17a52b2e8339d2989e0aba4f3735ab963c9c2
yoyu <yoyu@localhost.localdomain>
2021-03-07 22:32:05 +0800

    deleted:    api/:wq 	modified:   api/api.cpp 	deleted:    api/api.cpp.bak.bak 	modified:   api/c_call_lua/wk.lua 	deleted:    api/color.cpp.bak 	deleted:    api/color.h.bak 	deleted:    capi/Makefile 	deleted:    capi/b.lua 	deleted:    capi/lib1.lua 	deleted:    capi/test.c 	deleted:    capi/test.lua

--- lua/api/api.cpp
+++ lua/api/api.cpp
@@ -364,6 +364,25 @@ int table_next_test(const char*filename)
     return 1;
 }
 
+int test_lua_cmd(void) {
+    lua_State *L = luaL_newstate();   //打开 Lua
+    luaL_openlibs(L);                 //打开所有的标准库
+    char buff[256];
+    while (fgets(buff, sizeof(buff), stdin) != NULL) {
+        //1. 编译用户输入的每一行内容，并向栈中压入编译得到的函数
+        //2. 从栈中弹出编译后得到的函数，并以保护模式运行
+        int error = luaL_loadstring(L, buff) ||
+                    lua_pcall(L, 0, 0, 0);
+        //3. 错误信息处理：获取栈中的错误信息并打印，随后删除栈中的错误信息
+        if (error) { 
+            fprintf(stderr, "%s\n", lua_tostring(L, -1));
+            lua_pop(L, 1);
+        }
+    }
+    lua_close(L);
+    return 0;
+}
+
 int main(void)
 {
     //stackDumpTest();
@@ -385,6 +404,7 @@ int main(void)
     //dofile(filename);
     //char filename[] = "luatest.lua";
     //table_next_test(filename);
+    test_lua_cmd();
     char filename[] = "c_call_lua/wk.lua";
     callwk(filename, "wkmain");
     callwk(filename, "wkinit");
--- lua/api/c_call_lua/wk.lua
+++ lua/api/c_call_lua/wk.lua
@@ -1,19 +1,20 @@
 package.cpath = '/home/yoyu/hub/testcode/lua/api/?.so;'    --搜索so模块
 local lib = require "libtest"
 
-local hello= "Hello from wk.lua"
-print(hello)
-
 function wk(a, b)
     local res = lib.lsum(a, b)
     return res
 end
 
 function wkinit()
+    local hello= "Hello from wkinit in wk.lua"
+    print(hello)
     lib.lwkinit()
 end
 
 function wkmain()
+    local hello= "Hello from wkmain in wk.lua"
+    print(hello)
     lib.lwkmain()
 end
 
